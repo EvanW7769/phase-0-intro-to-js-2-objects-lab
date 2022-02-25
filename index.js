fd45e170acfd841c19a5de95734f8e18b64b12d5
Evan Watson <thomasevanwatson@gmail.com>
2022-02-25 10:48:09 -0500

    commit tree 3c4099c0e265dd3a7f5333c2e9b26de8ef399b46

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your solution in this file!
+let employee = {name: "evan",
+address: "123 Bliss"}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+const employeeUpdate = {...employee}
+employeeUpdate[key]=value;
+return employeeUpdate;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee
+}
